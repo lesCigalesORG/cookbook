4f20e3579309ecb7961c0735d66a14eba8c165f4
T0aD <julien.perez@epsylonia.net>
2014-04-06 11:37:33 +0200

    - added database config file

---
+++ app/config/database.php
@@ -0,0 +1,25 @@
+<?php
+
+class DATABASE_CONFIG {
+
+  var $default = array(
+    'driver' => 'mysql',
+    'persistent' => false,
+    'host' => 'localhost',
+    'login' => 'XXXX',
+    'password' => 'XXXX',
+    'database' => 'XXXX',
+    'prefix' => '',
+    'encoding' => 'utf8'
+  );
+  var $controlpanel = array(
+    'driver' => 'mysql',
+    'persistent' => false,
+    'host' => 'localhost',
+    'login' => 'XXXX',
+    'password' => 'XXXX',
+    'database' => 'XXXX',
+    'prefix' => '',
+    'encoding' => 'utf8'
+  );
+}
